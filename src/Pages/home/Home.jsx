6961bb029e6e269a481c980c2e7f4f0b1526d48c
suvendu sekhar sahoo <suvendusekharsahoo5@gmail.com>
2023-09-28 21:48:55 +0530

    slider component

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
+import { cards } from "../../data";
+import CatCard from "../../components/catCard/CatCard";
 
 const Home = () => {
   return (
     <div>
       <Featured />
+      <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => {
+          return <CatCard card={card} key={card.id} />;
+        })}
+      </Slide>
     </div>
   );
 };
